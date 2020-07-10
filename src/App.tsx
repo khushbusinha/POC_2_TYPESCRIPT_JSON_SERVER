93b8f26ffd826f2d3e208a59bc9e406bbf1d8514
Kumari Sinha <kumari.sinha@in.abb.com>
2020-07-10 15:36:31 +0530

    initial commit for json-server app

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { RouteComponentProps, Link, Switch, Route } from 'react-router-dom';
+import CreateCustomer from './components/customer/CreateCustomer';
+import Home from './components/Home';
+import EditCustomer from './components/customer/EditCustomer';
+
+const App: React.FC = () => {
+   return(
+       <div>
+           <nav>
+               <ul>
+                   <li>
+                       <Link to={'/'}> Home </Link>
+                   </li>
+                   <li>
+                       <Link to={'/create'}> Create Customer</Link> 
+                   </li>
+               </ul>
+           </nav>
+           <Switch>
+               <Route path={'/create'} exact component={CreateCustomer}/>
+               <Route path={'/'} exact component={Home}/>
+               <Route path={'/edit/:id'} exact component={EditCustomer} />
+           </Switch>
+       </div>
+   );
+}
+
+export default App;
